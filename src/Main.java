c91dc6ccd32bb357ad96b832079b10065889f542
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2023-02-25 15:32:23 +0300

    formatting3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-
         int balans = 100;
         int summ = 1100;
         int limit = 1000;
         int bonus = 0;
         int res = summ + balans;
+
         if (summ < limit) {
             bonus = 0;
         } else {
             bonus = summ / 100;
         }
+
         if (res > limit) {
             System.out.println("Сумма на счету: " + (res + bonus));
             System.out.println("Сумма бонусов: " + bonus);
